e145ed8f40afb5a243244e3351a79284a73790af
jhonatan velez <talimon@outlook.com>
2019-09-22 15:30:24 -0500

    semana tres pokedex

--- src/components/CaughtPokemon.js
+++ src/components/CaughtPokemon.js
@@ -4,8 +4,9 @@ import React, { Component, Fragment } from 'react'
 class CaughtPokemon extends Component {
 
     state = {
-        count: 0,
-        pokemonNameInput: ''
+
+        pokemonNameInput: '',
+        caughtPokemon: []
 
     }
 
@@ -22,23 +23,38 @@ class CaughtPokemon extends Component {
     }
 
     catchPokemon = () => {
-        this.setState(previousState => {
-            return {
-                count: previousState.count + 1
-            };
-        });
-    };
 
-    handleInputChange = () => {
+        const newCaughtPokemon = [...this.state.caughtPokemon]
+        const pokemonNameInput = this.state.pokemonNameInput
+        console.log(newCaughtPokemon)
+        newCaughtPokemon.push(pokemonNameInput)
+        this.setState({
+            caughtPokemon: newCaughtPokemon,
+            pokemonNameInput: ""
+        });
+
+
+        console.log(newCaughtPokemon)
 
     }
+
+    handleInputChange = (event) => {
+        event.preventDefault();
+        this.setState({
+            pokemonNameInput: event.target.value
+
+        });
+    }
+
+
 
     render() {
         return (
             <Fragment>
-                Count: {this.state.count}
+                Count: {this.state.caughtPokemon.length}
                 <button onClick={this.catchPokemon} >Catch Pokemon</button>
-                <input type="text" value={this.pokemonNameInput} />
+                <input type="text" value={this.state.pokemonNameInput} onChange={this.handleInputChange} />
+                <ul>{this.state.caughtPokemon.map(pokemon => <li>{pokemon}</li>)}</ul>
             </Fragment>
         );
     }
